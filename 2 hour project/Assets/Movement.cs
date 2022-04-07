032452b5420d01f053a218f8eaf7b009b60305a6
francisco <franciscosanchez2101@gmail.com>
2022-04-07 09:39:41 -0300

    Created project.

---
+++ 2 hour project/Assets/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    CharacterController player;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3();
+        movement.x = h * speed * Time.deltaTime;
+        movement.z = v * speed * Time.deltaTime;
+
+        player.SimpleMove(movement);
+    }
+}
